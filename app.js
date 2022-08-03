3a7eebbc09f678996c626fed946a945620259aab
Koni <konichan619@gmail.com>
2022-08-03 20:15:10 +0800

    Set up variables and functions

---
+++ app.js
@@ -0,0 +1,34 @@
+const addBookBtn = document.querySelector('.add-book');
+
+// An array that stores all the information for every book.
+let allBooks = [];
+
+let title = 'Book of Pandora',
+  author = 'Deez',
+  pages = 5;
+
+// An onlick  function that adds a book and its details which have been provided
+// by the user.
+function bookInfo(title, author, pages, isRead = false) {
+  this.title = title;
+  this.author = author;
+  this.pageCount = pages;
+  this.isRead = isRead;
+}
+
+function addBook() {
+  addBookBtn.onclick = function () {
+    allBooks.push(new bookInfo(title, author, pages));
+  };
+}
+
+addBook();
+
+// Modal open and close functionality
+const openModal = document.querySelector('.add-book');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.shadow');
+openModal.onclick = function () {
+  modal.style.display = 'flex';
+  overlay.style.display = 'block';
+};
