4966f482da17dd691ed31ecd91a1a82038c2e7f7
Koni <konichan619@gmail.com>
2022-08-04 20:50:21 +0800

    Add onclick function that pushes user input object inside array

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const addBookBtn = document.querySelector('.add-book');
-
 // An array that stores all the information for every book.
 let allBooks = [];
 
-let title = 'Book of Pandora',
-  author = 'Deez',
-  pages = 5;
+let title = document.getElementById('book-title'),
+  author = document.getElementById('author-name'),
+  pages = document.getElementById('page-count'),
+  isRead = '';
 
 // An onlick  function that adds a book and its details which have been provided
 // by the user.
+const addBookBtn = document.getElementById('add-book');
+
 function bookInfo(title, author, pages, isRead = false) {
   this.title = title;
   this.author = author;
@@ -16,13 +17,20 @@ function bookInfo(title, author, pages, isRead = false) {
   this.isRead = isRead;
 }
 
-function addBook() {
-  addBookBtn.onclick = function () {
-    allBooks.push(new bookInfo(title, author, pages));
-  };
-}
+addBookBtn.onclick = function () {
+  readStatus(document.getElementById('checkbox').checked);
+  allBooks.push(new bookInfo(title.value, author.value, pages.value, isRead));
+  console.log(allBooks);
+  resetInput();
+};
 
-addBook();
+function readStatus(checkBox) {
+  if (checkBox) {
+    isRead = true;
+  } else {
+    isRead = false;
+  }
+}
 
 // Modal open and close functionality
 const openModal = document.querySelector('.add-book');
